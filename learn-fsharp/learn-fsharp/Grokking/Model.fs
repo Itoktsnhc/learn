27ae3400be41a9cbbabd1d759625c2dca4dfc530
itoktsnhc <itoktsnhc@hotmail.com>
2021-04-26 16:29:35 +0800

    add some fsharp

---
+++ learn-fsharp/learn-fsharp/Grokking/Model.fs
@@ -0,0 +1,14 @@
+﻿module Model
+
+type UserId = int
+type TransactionId = int
+
+type CreditCard =
+    { Number: string
+      Expiry: string
+      Cvv: string }
+
+type User = 
+    { Id: UserId
+      CreditCard: CreditCard option
+      Limit: double option}
---
+++ learn-fsharp/learn-fsharp/Grokking/Two.fs
@@ -0,0 +1,12 @@
+﻿module Grokking.Two
+
+open Model
+
+let apply a f =
+    match f, a with
+    | Ok g, Ok x -> g x |> Ok
+    | Error e, Ok _ -> e |> Error
+    | Ok _, Error e -> e |> Error
+    | Error e1, Error e2 -> (e1 @ e2) |> Error
+    
+let validateCreditCard (card: CreditCard) : Result<CreditCard, string list> = Error [ ""; "" ]
