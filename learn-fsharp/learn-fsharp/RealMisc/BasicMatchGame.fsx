0a6329cc7e4dbc3142029f8e7ea4596a26af7576
itoktsnhc <itoktsnhc@hotmail.com>
2021-04-28 16:04:22 +0800

    add some

---
+++ learn-fsharp/learn-fsharp/RealMisc/BasicMatchGame.fsx
@@ -0,0 +1,23 @@
+﻿open System
+
+type LoginModel = { Username: string; Password: string }
+
+let adminList =
+    [ { Username = "Tom"; Password = "123" }
+      { Username = "Jerry"; Password = "456" } ]
+    |> Set.ofList
+
+let rec LoginRoute () =
+    printfn "Please input your Username"
+    let username = Console.ReadLine()
+    printfn "Please input your Password"
+    let pwd = Console.ReadLine()
+    let loginCred = { Username = username; Password = pwd }
+
+    match adminList |> Set.contains loginCred with
+    | true -> printfn "Ok!!!"
+    | false ->
+        printfn "wrong pwd or username!~!"
+        LoginRoute()
+
+LoginRoute()
