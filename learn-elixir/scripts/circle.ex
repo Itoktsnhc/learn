f6716c0b3a61eff49c45c37f308036927f059a1a
itoktsnhc <itoktsnhc@hotmail.com>
2021-05-18 16:26:57 +0800

    update

--- learn-elixir/scripts/circle.ex
+++ learn-elixir/scripts/circle.ex
@@ -1,10 +1,10 @@
 defmodule Circle do
   @moduledoc "Implements basic circle functions"
   @pi 3.14159
-
   @doc "Computes the area of a circle"
+  @spec area(number)::number
   def area(r), do: r*r*@pi
-
   @doc "Computes the circumference of a circle"
+  @spec circumference(number) :: number
   def circumference(r), do: 2*r*@pi
 end
---
+++ learn-elixir/scripts/geometry.ex
@@ -0,0 +1,7 @@
+defmodule Geometry do
+  def area(a), do: area(a,a)
+  def area(a, b), do: a*b
+  def fun(a,b\\1,c,d\\2) do
+    a+b+c+d
+  end
+end
