c040c60b9d76495e3fe785624b270bb91c63d6ad
itoktsnhc <itoktsnhc@hotmail.com>
2021-06-21 13:32:20 +0800

    daily problem

--- learn-algo/Learn.Algo/Learn.Algo.Problem/P_0.cs
+++ learn-algo/Learn.Algo/Learn.Algo.Problem/P_0.cs
@@ -8,10 +8,22 @@ namespace Learn.Algo.Problem
 {
     public partial class Solution
     {
-        //726: https://leetcode-cn.com/problems/number-of-atoms/
-        public string CountOfAtoms(string formula)
+        #region 401 https://leetcode-cn.com/problems/binary-watch/
+        public IList<string> ReadBinaryWatch(int t)
         {
+            /*
+             * 相当于求一个二进制数 最多10位 其中1的个数=t
+             * 然后满足时间需求的情况下的所有结果
+            */
+            var res = new List<string>();
+            void Dfs(int idx, int k)
+            {
 
+            }
+            Dfs(0, t);
+
+            return res;
         }
+        #endregion
     }
 }
