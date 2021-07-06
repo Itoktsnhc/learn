8ac0e1cda6a4940a4f4e495cf9cb4925bb200db6
itoktsnhc <itoktsnhc@hotmail.com>
2021-07-06 15:12:25 +0800

    use rust-analyzer

--- learn-rust/by_book_in_action/src/main.rs
+++ learn-rust/by_book_in_action/src/main.rs
@@ -1,16 +1,42 @@
 #![allow(unused)]
+#![allow(unused_variables)]
 
-use std::convert::TryInto;
+use std::fmt::Debug;
 use std::ops::Add;
+type File = String;
 
+fn report<T>(item: T)
+where
+    T: Debug,
+{
+    println!("{:?}", item);
+}
+
+fn open(f: &mut File) -> bool {
+    true
+}
+fn close(f: &mut File) -> bool {
+    true
+}
+
+#[allow(dead_code)]
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> bool {
+    unimplemented!()
+}
 
 fn main() {
+    main_3_1();
     //main_2_3();
     //main_2_4();
     //main_2_5_7();
-    main_2_22();
+    //main_2_22();
 }
+fn main_3_1() {
+    let mut f1 = File::from("f1.txt");
+    open(&mut f1);
 
+    close(&mut f1);
+}
 fn main_2_27() {
     let one = [1, 2, 3];
     let two: [u8; 3] = [1, 2, 3];
@@ -37,16 +63,14 @@ fn main_2_22() {
     println!("{}", res);
 }
 
-fn add_with_generic<T: Add<Output=T>>(i: T, j: T) -> T {
+fn add_with_generic<T: Add<Output = T>>(i: T, j: T) -> T {
     i + j
 }
-
 
 fn main_2_5_7() {
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
     for item in &haystack {
-        let res = match *item
-        {
+        let res = match *item {
             42 | 132 => "hit",
             _ => "miss",
         };
@@ -82,12 +106,18 @@ fn main_2_3() {
     let b: u16 = 100;
     if a < b.try_into().unwrap() {
         println!("true");
-    } else { println!("false"); }
+    } else {
+        println!("false");
+    }
     let absolute_difference: f64 = (0.3f64 - (0.1 + 0.2)).abs();
     assert!(absolute_difference <= f64::EPSILON);
-    assert_eq!(f32::NAN == f32::NAN, false);//NAN != NAN
+    assert_eq!(f32::NAN == f32::NAN, false); //NAN != NAN
 }
 
 fn is_even_2(a: i32) -> bool {
-    if a % 2 == 0 { true } else { false }
+    if a % 2 == 0 {
+        true
+    } else {
+        false
+    }
 }
