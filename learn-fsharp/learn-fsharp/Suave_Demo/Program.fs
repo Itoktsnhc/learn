3c1faad2135f50558a873f9769d5d2b3e3e9d209
itoktsnhc <itoktsnhc@hotmail.com>
2021-08-04 13:50:07 +0800

    add some suave code

--- learn-fsharp/learn-fsharp/Suave_Demo/Program.fs
+++ learn-fsharp/learn-fsharp/Suave_Demo/Program.fs
@@ -1,17 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 
-let app =
-    choose [ GET
-             >=> choose [ path "/hello" >=> OK "Hello Get"
-                          path "/goodbay" >=> OK "Good bye GET" ]
-             POST
-             >=> choose [ path "/hello" >=> OK "Hello POST"
-                          path "/goodbye" >=> OK "Good bye POST" ] ]
+let browseHome: WebPart =
+    path "/hello"
+    >=> choose [ RequestErrors.NOT_FOUND "Found no handlers" ]
+
+let requiresAuthentication _ =
+    choose [ GET >=> path "/public" >=> OK "Default GET"
+             // Access to handlers after this one will require authentication
+             Authentication.authenticateBasic
+                 (fun (user, pwd) -> user = "foo" && pwd = "bar")
+                 (choose [ GET
+                           >=> path "/whereami"
+                           >=> OK(sprintf "Hello authenticated person ")
+                           GET >=> path "/" >=> OK("Some path")
+                           GET >=> browseHome ]) ] // Serves file if exists
+
+let app = requiresAuthentication ""
 
 startWebServer defaultConfig app
