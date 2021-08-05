a8d551edccf4741006c87821e89669d923e56189
itoktsnhc <itoktsnhc@hotmail.com>
2021-08-05 12:15:05 +0800

    add input binding

--- learn-fsharp/elmish/src/App.fs
+++ learn-fsharp/elmish/src/App.fs
@@ -4,36 +4,21 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type State = { Count: int }
 
-type Msg =
-    | Increment
-    | Decrement
+type State = { TextInput: string }
+type Msg = SetTextInput of string
 
-let init () = { Count = 1 }
+let init () = { TextInput = "" }
 
-let update (msg: Msg) (state: State) : State =
+let update msg state =
     match msg with
-    | Increment -> { state with Count = state.Count + 1 }
+    | SetTextInput textInput -> { state with TextInput = textInput }
 
-    | Decrement -> { state with Count = state.Count - 1 }
-
-let render (state: State) (dispatch: Msg -> unit) =
-    Html.div [ Html.button [ prop.onClick (fun _ -> dispatch Increment)
-                             prop.text "+"
-                             prop.classes [ "button"; "is-primary" ] ]
-               Html.div state.Count
-               Html.button [ prop.onClick (fun _ -> dispatch Decrement)
-                             prop.text "-"
-                             prop.classes [ "button"; "is-primary" ] ]
-               Html.h1 [ prop.classes [ if state.Count < 0 then "hidden" ]
-                         prop.text (
-                             if state.Count % 2 = 0 then
-                                 "Count is even"
-                             else
-                                 "Count is odd"
-                         ) ] ]
+let render state dispatch =
+    Html.div [ Html.input [ prop.onChange (SetTextInput >> dispatch) ]
+               Html.span state.TextInput ]
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elm-app-bootstrap"
+
 |> Program.run
