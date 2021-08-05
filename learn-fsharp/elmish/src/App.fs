cb60b618b2ba29b2020c724c5a0c7aae5003c985
itoktsnhc <itoktsnhc@hotmail.com>
2021-08-05 11:38:50 +0800

    basic for elmish

--- learn-fsharp/elmish/src/App.fs
+++ learn-fsharp/elmish/src/App.fs
@@ -4,39 +4,36 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type State =
-    { Count: int }
+type State = { Count: int }
 
 type Msg =
     | Increment
     | Decrement
 
-let init() =
-    { Count = 0 }
+let init () = { Count = 1 }
 
-let update (msg: Msg) (state: State): State =
+let update (msg: Msg) (state: State) : State =
     match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }
+    | Increment -> { state with Count = state.Count + 1 }
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }
+    | Decrement -> { state with Count = state.Count - 1 }
 
 let render (state: State) (dispatch: Msg -> unit) =
-  Html.div [
-    Html.button [
-      prop.onClick (fun _ -> dispatch Increment)
-      prop.text "Increment"
-    ]
-
-    Html.button [
-      prop.onClick (fun _ -> dispatch Decrement)
-      prop.text "Decrement"
-    ]
-
-    Html.h1 state.Count
-  ]
+    Html.div [ Html.button [ prop.onClick (fun _ -> dispatch Increment)
+                             prop.text "+"
+                             prop.classes [ "button"; "is-primary" ] ]
+               Html.div state.Count
+               Html.button [ prop.onClick (fun _ -> dispatch Decrement)
+                             prop.text "-"
+                             prop.classes [ "button"; "is-primary" ] ]
+               Html.h1 [ prop.classes [ if state.Count < 0 then "hidden" ]
+                         prop.text (
+                             if state.Count % 2 = 0 then
+                                 "Count is even"
+                             else
+                                 "Count is odd"
+                         ) ] ]
 
 Program.mkSimple init update render
-|> Program.withReactSynchronous "elmish-app"
+|> Program.withReactSynchronous "elm-app-bootstrap"
 |> Program.run
