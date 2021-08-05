22912bad9ab0f1987a6a48f1c072d36a22370435
itoktsnhc <itoktsnhc@hotmail.com>
2021-08-05 14:04:17 +0800

    todo list part I

--- learn-fsharp/elmish/src/App.fs
+++ learn-fsharp/elmish/src/App.fs
@@ -3,36 +3,60 @@ module App
 open Elmish
 open Elmish.React
 open Feliz
-open Validation
 
-type State = { NumberInput: Validated<int> }
-type Msg = SetNumberInput of Validated<int>
+type State =
+    { TodoList: string list
+      NewTodo: string }
 
-let init () = { NumberInput = createEmpty () }
+type Msg =
+    | SetNewTodo of string
+    | AddNewTodo
+
+let init () =
+    { TodoList = [ "Learn F#" ]
+      NewTodo = "" }
 
 let update msg state =
     match msg with
-    | SetNumberInput numberInput -> { state with NumberInput = numberInput }
+    | SetNewTodo todoText -> { state with NewTodo = todoText }
+    | AddNewTodo when state.NewTodo = "" -> state
+    | AddNewTodo ->
+        { state with
+              NewTodo = ""
+              TodoList = List.append state.TodoList [ state.NewTodo ] }
 
-let tryParseInt (input: string) : Validated<int> =
-    try
-        success input (int input)
-    with
-    | _ -> failure input
+let appTitle =
+    Html.p [ prop.className "title"
+             prop.text "Elm To-Do List" ]
 
-let validatedTextColor validated =
-    match validated.Parsed with
-    | Some _ -> color.green
-    | None -> color.crimson
+let inputField (state: State) (dispatch: Msg -> unit) =
+    Html.div [ prop.classes [ "field"; "has-addons" ]
+               prop.children [ Html.div [ prop.classes [ "control"
+                                                         "is-expanded" ]
+                                          prop.children [ Html.input [ prop.classes [ "input"; "is-medium" ]
+                                                                       prop.valueOrDefault state.NewTodo
+                                                                       prop.onChange (SetNewTodo >> dispatch) ] ] ]
 
-let render state dispatch =
+                               Html.div [ prop.className "control"
+                                          prop.children [ Html.button [ prop.classes [ "button"
+                                                                                       "is-primary"
+                                                                                       "is-medium" ]
+                                                                        prop.onClick (fun _ -> dispatch AddNewTodo)
+                                                                        prop.children [ Html.i [ prop.classes [ "fa"
+                                                                                                                "fa-plus" ] ] ] ] ] ] ] ]
+
+let todoList (state: State) (dispatch: Msg -> unit) =
+    Html.ul [ prop.children [ for todo in state.TodoList ->
+                                  Html.li [ prop.classes [ "box"; "subtitle" ]
+                                            prop.text todo ] ] ]
+
+let render (state: State) (dispatch: Msg -> unit) =
     Html.div [ prop.style [ style.padding 20 ]
-               prop.children [ Html.input [ prop.valueOrDefault state.NumberInput.Raw
-                                            prop.onChange (tryParseInt >> SetNumberInput >> dispatch) ]
-                               Html.h1 [ prop.style [ style.color (validatedTextColor state.NumberInput) ]
-                                         prop.text state.NumberInput.Raw ] ] ]
+               prop.children [ appTitle
+                               inputField state dispatch
+                               todoList state dispatch ] ]
 
 Program.mkSimple init update render
-|> Program.withReactSynchronous "elm-app-bootstrap"
+|> Program.withReactSynchronous "elm-app"
 
 |> Program.run
--- learn-fsharp/elmish/src/Validation.fs
+++
@@ -1,7 +0,0 @@
-module Validation
-
-type Validated<'t> = { Raw: string; Parsed: Option<'t> }
-
-let createEmpty () : Validated<_> = { Raw = ""; Parsed = None }
-let success raw value : Validated<_> = { Raw = raw; Parsed = Some value }
-let failure raw : Validated<_> = { Raw = raw; Parsed = None }
