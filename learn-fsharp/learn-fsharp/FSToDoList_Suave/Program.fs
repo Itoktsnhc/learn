5c25d0b7049963301f123e991b7aa6e8f39919c1
itoktsnhc <itoktsnhc@hotmail.com>
2021-09-23 11:35:11 +0800

    add saturn_Demo

--- learn-fsharp/learn-fsharp/FSToDoList_Suave/Program.fs
+++ learn-fsharp/learn-fsharp/FSToDoList_Suave/Program.fs
@@ -1,10 +1,4 @@
 
 open System
 
-[<Literal>]
-let connectionString = 
-    @"Server=192.168.4.195;Port=3306;Database=hpcdb_v2;Uid=root;Pwd=123456;"
-
-
-let [<Literal>] resolutionPath = __SOURCE_DIRECTORY__ + @"/bin/"
 
