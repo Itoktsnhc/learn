d3a566b42d217847a49f1ea582262c9a9b54178a
itoktsnhc <itoktsnhc@hotmail.com>
2021-09-28 17:54:43 +0800

    add some code

---
+++ learn-fsharp/learn-fsharp/ForFunAndProfit/MapAndApply.fs
@@ -0,0 +1,60 @@
+﻿module ForFunAndProfit.MapAndApply
+
+module Option =
+    // The apply function for Options
+    let apply fOpt xOpt =
+        match fOpt, xOpt with
+        | Some f, Some x -> Some(f x) // 只有f 和 x 都存在
+        | _ -> None
+
+    let map f xOpt =
+        match xOpt with
+        | Some x -> f x |> Some
+        | _ -> None
+
+    let (<*>) = apply
+    let (<!>) = map
+
+    let lift2 f x y = f <!> x <*> y
+
+    let lift2'' f x y =
+        let mapped = map f x // (b->c) option
+        apply mapped y // apply ((b->c) option) b => c option
+
+    let lift3 f x y z = f <!> x <*> y <*> z
+
+    let lift3' f x y z =
+        let mapped = map f x //(b->c->d) option
+        let first = apply mapped y // apply ((b->c->d) option) b
+        apply first z
+
+let addPair x y = x + y
+let addPairOpt = Option.lift2 addPair
+
+// define a three-parameter function to test with
+let addTriple x y z = x + y + z
+
+// lift a three-param function
+let addTripleOpt = Option.lift3 addTriple
+
+Option.lift2 (+) (Some 2) (Some 3) |> ignore
+// define a tuple creation function
+let tuple x y = x,y
+
+// create a generic combiner of options
+// with the tuple constructor baked in
+let combineOpt x y = Option.lift2 tuple x y
+
+
+// alternate "zip" implementation
+// [f;g] apply [x;y] becomes [f x; g y]
+let rec zipList fList xList  =
+    match fList,xList with
+    | [],_
+    | _,[] ->
+        // either side empty, then done
+        []
+    | (f::fTail),(x::xTail) ->
+        // new head + new tail
+        (f x) :: (zipList fTail xTail)
+// has type : ('a -> 'b) -> 'a list -> 'b list
--- learn-fsharp/learn-fsharp/HowToDI/Program.fs
+++ learn-fsharp/learn-fsharp/HowToDI/Program.fs
@@ -1,12 +1,55 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
+module Domain =
+    type UserId = UserId of int
+    type UserName = string
+    type EmailAddress = EmailAddress of string
 
-// Define a function to construct a message to print
-let from whom = sprintf "from %s" whom
+    type Profile =
+        { UserId: UserId
+          Name: UserName
+          EmailAddress: EmailAddress }
 
+    type EmailMessage = { To: EmailAddress; Body: string }
 
+module Infrastructure =
+    open Domain
+
+    type ILogger =
+        abstract Info : string -> unit
+        abstract Error : string -> unit
+
+    type InfrastructureError =
+        | DbError of string
+        | SmtpError of string
+
+    type DbConnection = DbConnection of unit
+
+    type IDbService =
+        abstract NewDbConnection : unit -> DbConnection
+        abstract QueryProfile : DbConnection -> UserId -> Async<Result<Profile, InfrastructureError>>
+        abstract UpdateProfile : DbConnection -> Profile -> Async<Result<unit, InfrastructureError>>
+
+    type SmtpCredentials = SmtpCredentials of unit
+
+    type IEmailService =
+        abstract SendChangNotification : SmtpCredentials -> EmailMessage -> Async<Result<unit, InfrastructureError>>
+
+type ReaderM<'dependency, 'result> = 'dependency -> 'result
+
+let run (d: 'dependency) (r: ReaderM<'dependency, 'result>) : 'result = r d
+
+let map (f: 'a -> 'b) (r: ReaderM<'d, 'a>) : ReaderM<'d, 'b> = r >> f
+
+let (<?>) = map
+
+let apply (f: ReaderM<'d, 'a -> 'b>) (r: ReaderM<'d, 'a>) : ReaderM<'d, 'b> =
+    fun dep ->
+        let f' = run dep f
+        let a = run dep r
+        f' a
+
+let (<*>) = apply
 
 [<EntryPoint>]
-let main argv =
-    0 // return an integer exit code
+let main argv = 0 // return an integer exit code
