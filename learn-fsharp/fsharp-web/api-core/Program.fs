c7ccc68382702c145b6cadb111752b75b5e4bbf9
itoktsnhc <itoktsnhc@hotmail.com>
2021-12-31 16:37:07 +0800

    some

--- learn-fsharp/fsharp-web/api-core/Program.fs
+++ learn-fsharp/fsharp-web/api-core/Program.fs
@@ -2,19 +2,9 @@ namespace api_core
 
 #nowarn "20"
 
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
 
 module Program =
     let exitCode = 0
@@ -26,9 +16,11 @@ module Program =
         builder.Services.AddHttpClient()
         builder.Services.AddControllers()
         builder.Services.AddSwaggerGen()
+
+
         let app = builder.Build()
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwagger()
+        app.UseSwaggerUI()
 
         app.MapControllers()
         app.Run()
